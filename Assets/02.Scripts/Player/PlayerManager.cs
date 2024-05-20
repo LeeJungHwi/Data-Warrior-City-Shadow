97a412209d359a49596dbf130a06a750c401a01e
gdevhun <wjh9330@naver.com>
2024-05-20 20:52:55 +0900

    Feat: PlayerManager, Aim, Cam

--- Assets/02.Scripts/Player/PlayerManager.cs
+++ Assets/02.Scripts/Player/PlayerManager.cs
@@ -9,7 +9,7 @@ public class PlayerManager : MonoBehaviour
     private StarterAssetsInputs _input;
 
     [Header("Aim")] [SerializeField] private CinemachineVirtualCamera _aimCam;
-    
+    [SerializeField] private GameObject _aimImage;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +19,20 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AimCheck();
+    }
+
+    private void AimCheck() //코로스헤어와 에임캠 활성화 함수
+    {
         if (_input.aim)
         {
             _aimCam.gameObject.SetActive(true);
+            _aimImage.SetActive(true);
         }
         else
         {
             _aimCam.gameObject.SetActive(false);
+            _aimImage.SetActive(false);
         }
     }
 }
