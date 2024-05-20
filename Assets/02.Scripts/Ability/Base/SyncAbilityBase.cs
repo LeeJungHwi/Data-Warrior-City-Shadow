fa35e00e86184bb28ba0c2685d2783da7156db3a
DongHun, Won <150761282+gdevhun@users.noreply.github.com>
2024-05-20 21:47:30 +0900

    Revert "Feature/charmove"

--- Assets/02.Scripts/Ability/Base/SyncAbilityBase.cs
+++
@@ -1,5 +0,0 @@
-public class SyncAbilityBase : AbilityBase
-{
-    // 동기 스킬
-    public virtual void Cast() {}
-}
--- Assets/02.Scripts/Ability/Collision/Enemy.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    // 스킬 충돌처리 테스트용
-}
